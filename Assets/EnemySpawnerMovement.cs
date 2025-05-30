18b70198a848923bdf2f213d6b8d121c9bafe3f0
elijl <eli.jl.dew@gmail.com>
2025-05-30 10:54:23 -0400

    Made the enemy spawner move around the screen

--- Assets/EnemySpawnerMovement.cs
+++ Assets/EnemySpawnerMovement.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 
 public class EnemySpawnerMovement : MonoBehaviour
 {
-    private float[][] _travelToList;
+    [SerializeField] private float _enemySpawnerMoveSpeed;
+    [SerializeField] private float _distanceToChangeTargetPoint;
 
+    private int _nextPoint = 1;
+
+    private float[,] _travelToList = new float[12, 2]
+    {
+        {0, 4},
+        {3.3f, 2.6f},
+        {6.6f, 1.3f},
+        {10, 0},
+        {6.6f, -1.3f},
+        {3.3f, -2.6f},
+        {0, -4},
+        {-3.3f, -2.6f},
+        {-6.6f, -1.3f},
+        {-10, 0},
+        {-6.6f, 1.3f},
+        {-3.3f, 2.6f},
+    };  
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(_travelToList[_nextPoint, 0], _travelToList[_nextPoint, 1], 0), _enemySpawnerMoveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, new Vector3(_travelToList[_nextPoint, 0], _travelToList[_nextPoint, 1], 0)) <= _distanceToChangeTargetPoint)
+        {
+            Debug.Log(_nextPoint);
+            if (_nextPoint == _travelToList.GetLength(0) - 1)
+            {
+                _nextPoint = 0;
+            }
+            else
+            {
+                _nextPoint++;
+            }
+        }
     }
 }
